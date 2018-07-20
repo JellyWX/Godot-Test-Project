26380cec399a4954206081fe750a533236bc3e0a
jude-LafiteIII <judewrs@gmail.com>
2018-07-20 17:13:54 +0100

    moved player and progressor to their own instances

--- Progessor.gd
+++ Progessor.gd
@@ -1,8 +1,10 @@
 extends Area2D
 
+export(String, FILE, '*.tscn') var next_level
+
 func _ready():
     connect('body_entered', self, 'on_body_entered')
 
 func on_body_entered(body):
     if body.get_name() == 'Player':
-        print('Player entered space')
+        get_tree().change_scene(next_level)
